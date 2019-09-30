4337fb86f03f1c67a8b2f88b65cc05b25b157934
Abel Varga <avarga@hajtospincer.hu>
2019-09-30 23:44:46 +0200

    Communicate tabs to popup

--- src/background/main.js
+++ src/background/main.js
@@ -1,1 +1,77 @@
-console.log('OK')
+import r from "redda/src"
+
+const state = r.state()
+
+const tab_exists = tab => state.get().tab_list.find(({ id }) => id == tab.id)
+
+const tab_list = () => []
+const upsert_tab = (tab_list, tab) => {
+  const existing = tab_exists(tab)
+
+  if (existing) return tab_list.map(ex => (ex.id == tab.id ? tab : ex))
+
+  return [...tab_list, tab]
+}
+
+const rm_tab = (tab_list, tab) => tab_list.filter(({ id }) => tab.id != id)
+
+state.add(tab_list, upsert_tab, rm_tab)
+
+const active_tab = () => null
+const set_active = (_, tab_id) => tab_id
+
+state.add(active_tab, set_active)
+
+const store_tab = ({ id, windowId, url, is_youtube, is_active } = {}) => {
+  if (!id) return
+
+  state.disp(upsert_tab, {
+    id,
+    window_id: windowId,
+    is_youtube:
+      is_youtube == undefined ? !!(url && url.match(/youtube/)) : is_youtube,
+    is_active: !!is_active
+  })
+
+  state.disp(set_active, id)
+}
+
+const send_active_tab = () => {
+  const curr = state.get()
+  const tab = curr.tab_list.find(({ id }) => id == curr.active_tab)
+
+  chrome.runtime.sendMessage(ext_id, {
+    action: "set_active",
+    active_tab: curr.tab_list.find(({ id }) => tab.id == id)
+  })
+}
+
+const ext_id = chrome.runtime.id
+
+window.s = state
+
+const init = () => {
+  state.on_change(send_active_tab)
+
+  chrome.tabs.query({ active: true }, ([tab]) => store_tab(tab))
+
+  chrome.tabs.onActivated.addListener(({ tabId } = {}) => {
+    if (!tabId) return
+
+    chrome.tabs.get(tabId, tab => ({
+      ...store_tab,
+      ...(tab_exists(tab) || {})
+    }))
+  })
+
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log(message)
+    if (sender.id != chrome.runtime.id) return
+
+    if (message.action == "set_active") return store_tab(message.active_tab)
+
+    if (message.action == "get_active") return send_active_tab()
+  })
+}
+
+init()
--- src/popup/main.js
+++ src/popup/main.js
@@ -2,33 +2,59 @@ import r from "redda/src"
 const { nav, div, a, input, label, ul, li, span, img } = r.dom
 
 const state = r.state()
-const is_on = () => false
-const toggle = prev => !prev
 
-state.add(is_on, toggle)
+const active_tab = () => null
+const set_tab = (_, tab) => tab
+const set_tab_active = (tab, is_active) => {
+  const active_tab = { ...tab, is_active }
+
+  chrome.runtime.sendMessage(ext_id, { action: "set_active", active_tab })
+
+  return active_tab
+}
+
+state.add(active_tab, set_tab, set_tab_active)
+
+const is_on = () => false
+const set_on = (_, val) => val
+const toggle = prev => {
+  const is_active = !prev
+
+  state.disp(set_tab_active, is_active)
+
+  return is_active
+}
+
+state.add(is_on, set_on, toggle)
 
 const switcher = state.conn(
-  ({ is_on }) => [
-    div,
-    {
-      class: "switch center-align"
-    },
-    [
-      label,
-      "Off",
+  ({ is_on, active_tab }) => {
+    active_tab = active_tab || {}
+
+    return [
+      div,
+      {
+        class: "switch center-align"
+      },
       [
-        input,
-        {
-          type: "checkbox",
-          checked: is_on ? "checked" : null,
-          onclick: () => state.disp(toggle)
-        }
-      ],
-      [span, { class: "lever" }],
-      "On"
+        label,
+        "Off",
+        [
+          input,
+          {
+            type: "checkbox",
+            disabled: !active_tab.is_youtube ? "disabled" : null,
+            checked: is_on ? "checked" : null,
+            onclick: active_tab.is_youtube ? () => state.disp(toggle) : null
+          }
+        ],
+        [span, { class: "lever" }],
+        "On"
+      ]
     ]
-  ],
-  is_on
+  },
+  is_on,
+  active_tab
 )
 
 const title_bar = () => [
@@ -82,3 +108,23 @@ const app_cont = document.getElementById("app-cont")
 const render_app = r.render(app_cont, [app])
 
 state.on_change(render_app)
+
+window.s = state
+
+const store_tab = tab =>
+  tab && (state.disp(set_tab, tab), state.disp(set_on, !!tab.is_active))
+
+const ext_id = chrome.runtime.id
+
+const init = () => {
+  chrome.runtime.onMessage.addListener((message, sender) => {
+    console.log(message)
+    if (sender.id != chrome.runtime.id) return
+
+    if (message.action == "set_active") store_tab(message.active_tab)
+  })
+
+  chrome.runtime.sendMessage(ext_id, { action: "get_active" })
+}
+
+init()
